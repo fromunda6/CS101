0df8f46672d1e965d50e105a23f9a63c75de2d37
Goyo <fromunda6@zoho.com>
2016-06-14 19:14:55 -0600

    branch for cs materials

---
+++ unit1.py
@@ -0,0 +1,18 @@
+# write a program that prints out the number of
+# minutes there are in seven weeks
+print (60*24*7*7)
+
+# how far, in cm, does light travel in one nansecond?
+print ((299792458*100)/1000000000)
+
+# variable assignment example
+speed_of_light = 299792458 # meters per second
+billionth = 1.0/1000000000
+nanostick = speed_of_light * billionth * 100
+print (nanostick)
+cycles_per_second = 2700000000 # or 2.7ghz
+
+# ? How far does light travel, in metres, in a single computer cycle?
+
+print (speed_of_light/cycles_per_second)
+

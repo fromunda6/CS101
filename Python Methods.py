7062465b85bd7d8a9399d33860ac0fdaa65c1102
Goyo <fromunda6@zoho.com>
2016-07-26 12:38:34 -0600

    expansion of annotation 7.26.
    that

--- Python Methods.py
+++ Python Methods.py
@@ -1,25 +1,33 @@
 Python Methods learned thus far (a living reference):
 
-# create and assign a value to a variable
-var_name = constant, function, range, series, (...anything?)
+	# create and assign a value to a variable
+	var_name = constant, function, range, series, (...anything?)
 
-# return a character in position[#] from variable
-var_name[0]
-# ret_Char from position[#] to the end of var
-var_name[0:]
-# ret_Char before position[#]
-var_name[:5]
+	# return a character in position[#] from variable theta
+	theta[0]
+	# ret_Char from position[#] to the end of vari
+	vari[0:]
+	# ret_Char before position[#]
+	var_name[:5]
 
-# find and return the position of a substring in a string, with optional param = position at which to start searching
-var_name.find("certain string", 4)
-sea
-# display a variable in ui
-print (var_name)
+	# find and return the position of a substring in a string, with optional param = position at which to start searching
+	var_name.find("certain string", 4)
+	sea
+	# display a variable in ui
+	print (var_name)
 
-# define a procedure
-def proc_name(input1, input2):
-	task1 = var_name.find("certain string")
-	task2 = task1[2:4]
-	return output1, output2 #must be something computed by the procedure, i.e. task1, task2
+	# define a procedure
+	def proc_name(input1, input2):
+		task1 = var_name.find("certain string")
+		task2 = task1[2:4]
+		return output1, output2 #must be something computed by the procedure, i.e. task1, task2
 
+Python Color-Coding:
 
+# 1). recognized non-string datatypes (numbers and boolean values, maybe othere(?)) are in purple:
+
+	3, 2500, True, False #boolean values are only recognized in uppercase
+
+# 2). strings, besides the obvious '' and "" identifiers, will always show up in
+
+	"pale yellow"
--- unit2.py
+++ unit2.py
@@ -136,9 +136,242 @@ if <test expression> :
 	<block> #operations to perform if test expression evaluates to true, go here
 i=21 #next left-justfied code will run regardless of the outcome of <test expression>
 	def absoluteValue(x):
-		if x<0
+		if x<0:
 			x = -x
 		return x
+
+# quiz - define a procedure, "bigger", that takes in two numbers as inputs, and outputs the greater of the
+# two inputs
+
+def bigger(x,y):
+	if x > y
+		return x
+	if x < y
+		return y
+	if x=y
+		return "neither"
+
+bigger(2,6)
+
+# or, per teacher Dave:
+
+def bigger(x,y):
+	if x > y:
+		return x
+	return b
+
+	# or
+
+def bigger(x,y):
+	if x > y:
+		return x
+	else:
+		return y
+
+# else allows for a single return statement when the <block> is used to assign an addt'l VAR
+
+def bigger(x,y):
+	if x>y:
+		r=a
+	else:
+		r=b
+	return r
+
+# def a proc that takes a string as its input and outputs a boolean (logical,T/F) indicating whether
+# the input string is the name of a friend, where friends' names begin with 'D'
+
+def is_friend(name):
+	if name[0]=='D':
+		return True
+	else:
+		return False
+is_friend("dave")
+
+# or, we can return the result of the equality test directly, w/o an if statement, like so:
+
+def is_friend(name):
+	return name[0] == 'D'
+
+# define a similar proc such that names beginning with either 'D' or 'N'
+
+def is_friend(name):
+	return name[0] in ('D','N')
+
+print is_friend("Ned")
+
+# or assuming not having knowledge of "in" statement
+
+def is_friend(name):
+	if name[0] == 'D':
+		return True
+	else:
+		if name[0] == 'N':
+			return True
+		else:
+			return False
+
+# or, using or:
+
+def is_friend(name):
+	return name[0] == 'D' or name[0] == 'N'
+
+print (is_friend)('Doug')
+print (is_friend)('Nicole')
+print (is_friend)('Fred')
+
+# to illustrate the logic, and keeping in mind that boolean expressions always check for 'truthness' in Python, at least :
+
+print (True or False) # are either of the values 'True' or 'False' == True?
+print (False or True)
+print (True or True)
+print (False or False)
+
+# An 'or' expression will only evaluate as much as it needs to, e.g.:
+
+print (True or this_is_an_error) #returns 'True'
+print (False or this_is_an_error) #returns error/undefined
+
+# super Quiz: def a proc, biggest, that takes 3 numbers as inputs, and outputs the greatest of the 3 numbers:
+
+def biggest(x,y,z):
+	if x > y:
+		if x > z:
+			return x
+		else:
+			return z
+	else:
+		if y > z:
+			return y
+		else:
+			return z
+
+# make sure this ^^ terminates, otherwise, "you've got a Dead Baby, and you get an F for the semester"
+
+# or, using your 'bigger' procedure, which "composes two calls to proc 'bigger'" like so:
+
+def biggest(x,y,z):
+	return bigger(bigger(a,b),c)
+
+or max() is True def biggest()
+
+# Astounding truly, is that you could write every possible computer program with just the things we've seen up to this point
+
+# specifically, procedures, arithmetic, comparisons and if statements are sufficient to simulate the computing capabilities of any machine
+
+# --------------------------------------------------------------------------------------------------------------------------------------- #
+
+# Loops, while, gen syntax:
+	while <test expression>: #as in, while the test expression is true, perform statements in <block>; this will loop endlessly in absence of a false result
+		<block>
+
+# EX:
+
+i = 0
+while i<10:
+	print i #the function needs the value of 'i' within the block...
+	i = i + 1
+
+# quiz: what does this program do?
+i = 0
+while i!=10:
+	i=i+1
+	print (i)
+
+# def a proc, 'print_numbers', that takes as input a positive integer, and prints out all the whole numbers from 1 to the input number
+# toolbox: procedure, while loop, and assignment(new var)
+
+def print_numbers(d):
+	i=1
+	while i <= d:
+		print (i) #the order of print w.r.t. operations changing your iterator matters ; if you had print after operation '1' would not be printed in call
+		i = i + 1
+print (print_numbers(7))
+
+# Coding a factorial operation ie 5! = (5*4*3*2*1)
+
+def factorial(x):
+	result = 1
+	while x >= 1:
+		result = result * x
+		x = x - 1
+	return result
+
+	print factorial(4)
+
+# ... ugh
+
+# break statement gives us a way to stop a loop regardless of while's <test expression> being met
+# general syntax:
+
+while <testExpression>:
+	<Code>
+	if <BreakTest>:
+		break
+	<not executed if breakTest is met>
+<executed if breakTest is met>
+
+# again , the goal of today's unit, to write the code that will extract all links from a webpage:
+
+page = ...contents of some web page...
+
+start_link = page.find ('<a href=')
+start_quote = page.find ('"', start_link)
+end_quote = page.find ('"', start_quote+1)
+url = page[start_quote+1 : end_quote]
+print url
+page = page [end_quote:]
+
+start_link = page.find ('<a href=')
+
+def get_next_target (page):
+	start_link = page.find ('<a href=')
+	start_quote = page.find ('"', start_link)
+	end_quote = page.find ('"', start_quote+1)
+	url = page[start_quote+1 : end_quote]
+	return url, end_quote # in order to return two values, must make use of multiple assignment, via
+
+url, endpos = get_next_target(page) #assigns to url, endpos(ition) the output of the proc url, end_quote, respectively
+print url
+
+
+s , t = t , s #this is useful for swapping values that cannot be done in two sequential assignment statements,
+# since:
+
+s = t
+t = s
+
+# changes s to the value of t and does not change the value of t
+
+# what our function is missing is instructions on how to respond to an absence of any links (below: 'if url: ... else: break'), and also a way to stop looking
+# (below: 'if page.find == -1: return None, 0')
+
+def get_next_target (page):
+	start_link = page.find ('<a href=')
+	if page.find == -1:
+		return None, 0
+	start_quote = page.find ('"', start_link)
+	end_quote = page.find ('"', start_quote+1)
+	url = page[start_quote+1 : end_quote]
+	return url, end_quote
+
+
+def print_all_links (page):
+	while True:
+		url, endpos = get_next_target(page)
+		if url:
+			print url
+			page=page[endpos:]
+		else:
+			break
+
+# Main Concepts so far
+# unit 1 : variables, arithmetic
+# unit 2 : procedures, if, while
+
+
+
+
+
 
 
 
